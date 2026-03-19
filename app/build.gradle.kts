fc90fae0ea6db1bcd15eeb11fe09764c78293da9
Theo <marcinlowercase@gmail.com>
2026-03-18 19:55:27 -0600

    50 characters package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.webapk.app0000"
+        applicationId = "com.webapk.application.template.placeholder.namexx"
         minSdk = 29
         targetSdk = 36
         versionCode = 1
