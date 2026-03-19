645c4cd22abb6ccf90afe299a04e2ad04b44acaa
Theo <marcinlowercase@gmail.com>
2026-03-18 18:27:22 -0600

    passed icon url

--- app/src/main/java/com/webapk/app0000/MainActivity.kt
+++ app/src/main/java/com/webapk/app0000/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import org.json.JSONObject
 import androidx.core.net.toUri
+import kotlin.system.exitProcess
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +22,11 @@ class MainActivity : Activity() {
                 val url = jsonObj.getString("url")
                 val hostPackage = jsonObj.optString("host", "marcinlowercase.a")
                 val profileId = jsonObj.optString("profileId", "")
+                val pwaIconUrl = jsonObj.optString("iconUrl", "")
 
                 val intent = Intent(Intent.ACTION_VIEW, url.toUri())
                 intent.putExtra("is_pwa", true)
+                intent.putExtra("pwa_icon_url", pwaIconUrl)
 
                 if (profileId.isNotEmpty()) {
                     intent.putExtra("profileId", profileId)
@@ -32,7 +35,7 @@ class MainActivity : Activity() {
                 intent.setPackage(hostPackage)
 
                 // Launch the Browser directly on top of this Shell App
-                startActivity(intent)
+                startActivityForResult(intent, 1)
             } catch (e: Exception) {
                 e.printStackTrace()
                 finish() // Only close if something crashed
@@ -44,5 +47,12 @@ class MainActivity : Activity() {
         // By keeping this invisible Shell App alive at the bottom of the stack,
         // the Android OS natively considers the Shell App to be the "owner" of this window.
         // It will permanently use the PWA's true Name and Icon in the Recent Apps screen automatically!
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // When the user presses "Back" in the Browser, it finishes and returns here.
+        // Now the Shell App knows it's time to die, and takes the entire Task with it securely!
+        finishAndRemoveTask()
+        exitProcess(0)
     }
 }
